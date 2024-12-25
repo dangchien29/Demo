e4610eea45cf4555ddce0c8eb729a978d85708ec
Chien Dang <chiendmpd09434@fpt.edu.vn>
2024-12-25 17:25:48 +0700

    Initial commit

---
+++ duanmau/site/layout/footer.php
@@ -0,0 +1,52 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-3 col-lg-4 col-xl-3">
+            <h5>Về chúng tôi</h5>
+            <hr class="bg-white mb-2 mt-0 d-inline-block mx-auto w-25">
+            <p class="mb-0">
+                MinhChienShop - Trang thương mại điện tử lớn nhất Việt Nam
+            </p>
+        </div>
+
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto">
+            <h5>Thông tin</h5>
+            <hr class="bg-white mb-2 mt-0 d-inline-block mx-auto w-25">
+            <ul class="list-unstyled">
+                <li><a href=""><i class="fas fa-map-marker-alt"></i> Hòa Minh - Liên Chiểu </a></li>
+                <li><a href=""><i class="fas fa-phone-volume"></i> 0328232570</a></li>
+                <li><a href=""><i class="fas fa-envelope"></i>chiendmpd09434@fpt.edu.vn</a></li>
+            </ul>
+        </div>
+
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto">
+            <h5>Chính sách</h5>
+            <hr class="bg-white mb-2 mt-0 d-inline-block mx-auto w-25">
+            <ul class="list-unstyled">
+                <li><a href="">Chính sách bảo hành</a></li>
+                <li><a href="">Chính sách đổi hàng</a></li>
+                <li><a href="">Chính sách bảo mật</a></li>
+                <li><a href="">Chính sách vận chuyển</a></li>
+            </ul>
+        </div>
+
+        <div class="col-md-3 col-lg-3 col-xl-3">
+            <h5>Liên hệ</h5>
+            <hr class="bg-white mb-2 mt-0 d-inline-block mx-auto w-25">
+            <ul class="list-unstyled">
+                <li><i class="fa fa-home mr-2"></i> Công ty</li>
+                <li><i class="fa fa-envelope mr-2"></i> chiendmpd09434@fpt.edu.vn</li>
+                <li><i class="fa fa-phone mr-2"></i> + 0328232570</li>
+                <li><i class="fa fa-print mr-2"></i> + 0328232570</li>
+            </ul>
+        </div>
+        <!-- <div class="col-12 copyright mt-3">
+            <p class="float-left">
+                <a class="btn btn-info" href="<?= $ROOT_URL ?>">Về đầu trang</a>
+            </p>
+            <p class="text-right text-muted">Dự án mẫu của <i class="fa fa-heart red"></i>
+                <a href="#"><i>quantmpd09358</i></a>
+                <i class="fa fa-heart red"></i>
+            </p>
+        </div> -->
+    </div>
+</div>
---
+++ duanmau/site/trang-chinh/gioi-thieu.php
@@ -0,0 +1,53 @@
+<div class="wrapper" id="wrapper">
+    <div class="" id="banner">
+        <div class="parallax text-center" style="background-image:url(../../content/images/banners/b.webp);">
+            <div class="parallax-pattern-overlay">
+                <div class="container text-center text-white" style="height:650px;padding-top:15%;">
+                    <h3 class="learn shadow font-weight-bold">Về chúng tôi</h3>
+                    <h2 class="display-2 shadow font-weight-bold">MinhChienShop</h2>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <section class="aboutus" id="aboutus">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="heading text-center">
+                        <img class="dividerline"
+                            src="http://www.pixempire.com/images/preview/horizontal-line-icon-1.jpg" alt="">
+                        <h2 class="text-danger">Siêu thị trực tuyến lớn trong Việt Nam</h2>
+                        <img class="dividerline"
+                            src="http://www.pixempire.com/images/preview/horizontal-line-icon-1.jpg" alt="">
+                        <h4>Thế giới ngày nay đang bước vào cuộc cách mạng công nghiệp 4.0 với nhiều những cải tiến về công nghệ thông tin.
+                             Điều này đồng nghĩa với tầm quan trọng của máy vi tính ngày càng được đề cao.
+                              Chiếc máy vi tính đã trở thành người bạn thân thiết với mọi nhà và là người bạn đồng hành không thể thiếu với mỗi học sinh chúng ta.
+
+                            Ở máy tính xách tay bàn phím và chuột không được tách rời như máy tính để bàn mà được gắn liền vào máy rất gọn nhẹ.
+                            Thân máy gồm ram, ổ đĩa, nơi cắm loa, tai nghe hoặc USB. Đây là bộ phận tối quan trọng của máy vi tính vì nó là nơi xử lý thông tin dữ liệu rất tinh vi.
+                            Máy tính có bộ nhớ lớn phục vụ cho nhu cầu lưu trữ thông tin nên người dùng có thể tải ảnh, video hay phần mềm trực tiếp về máy.
+
+                            Bên cạnh đó chúng ta cũng cần để ý đến những phần mềm lỗi chứa virus sẽ gây hại cho máy tính.
+                            Máy tính hiện đại không còn cồng kềnh nữa mà ngày càng được thiết kế nhỏ gọn giúp chúng ta có thể gấp lại và đem theo rất thuận tiện.
+                            Máy tính để bàn, máy tính xách tay rồi đến máy tính bảng là những sản phẩm khẳng định sự phát triển vượt bậc của công nghệ và trí óc, tính sáng tạo của con người..</h4>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <div class="container mt-5">
+            <div class="row">
+                <div class="col">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d821.7360783934286!2d108.16950381012475!3d16.052377229394036!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3142196d9a203685%3A0x4e8027fe58d65525!2zQ2FvIMSR4bqzbmcgRlBUIEPGoSBT4bufIDI!5e1!3m2!1sen!2sus!4v1696840893293!5m2!1sen!2sus"
+                     width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+            </div>
+
+        </div>
+
+    </section>
+
+
+</div>
